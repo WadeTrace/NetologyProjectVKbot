862c547911929ddc1c69a9f9367b5c5b0beca24f
Ivanov Nikita <destira@mail.ru>
2022-12-10 21:07:12 +0300

    token

--- vkapi.py
+++ vkapi.py
@@ -1,6 +1,6 @@
 import vk_api
 
-token = ""
+token = "vk1.a.uvSWdZR6WWdEIOHoTLCI3x77cUVvGo1KCoIgo6iegN2edSNYCmmNBHht1FPs1KdbInQ16S5RP2Cl6PBJ89lQVFzWOflLk4rO1xlfv6AIRf_zp_1f256PJgpe4AjKmusezdsKdF6PuX7VcvDmD8hw1tqg9FzYhqxQ5WsEybDf1-n9KEnOB96CWD6Ln6eK4_TlINy5HKANPJwgQjmSB9gqsA"
 
 vk = vk_api.VkApi(token=token)
 
