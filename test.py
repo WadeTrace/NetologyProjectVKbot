fb3e7226e0905eb0601c9a461892fdc3f68b9a75
Ivanov Nikita <destira@mail.ru>
2022-12-11 18:19:04 +0300

    Ккrequest_of_data

---
+++ test.py
@@ -0,0 +1,72 @@
+from random import randrange
+
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+token = ""
+
+vk = vk_api.VkApi(token=token)
+longpoll = VkLongPoll(vk)
+
+
+def write_msg(user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+
+
+params = {}
+k = 0
+sex = ['мужской', 'женский']
+city = ['москва']
+ages = [10, 90]
+
+for event in longpoll.listen():
+
+    if event.type == VkEventType.MESSAGE_NEW:
+        # print(dir(event))
+        if event.to_me:
+            request = event.text
+            if request == "Начать":
+                k = 0
+                write_msg(event.user_id, f"Привет")
+                write_msg(event.user_id, f"Введите пол")
+                k += 1
+            elif k == 1:
+                if request.lower() in sex:
+                    params["sex"] = request
+                    print(params["sex"])
+                    write_msg(event.user_id, f"Введите город")
+                    k += 1
+                else:
+                    write_msg(event.user_id, f"Неверно введено значеие, введите заново")
+            elif k == 2:
+                if request.lower() in city:
+                    params["city"] = request
+                    print(params["city"])
+                    write_msg(event.user_id, f"Введите минимальный возраст")
+                    k += 1
+                else:
+                    write_msg(event.user_id, f"Неверно введено значеие, введите заново")
+            elif k == 3:
+                try:
+                    years = int(request)
+                    if years >= ages[0] and years <= ages[1]:
+                        params["min_age"] = years
+                        print(params["min_age"])
+                        write_msg(event.user_id, f"Введите максимальный возраст")
+                        k += 1
+                    else:
+                        write_msg(event.user_id, f"Неверно введено значеие, введите заново")
+                except ValueError:
+                    write_msg(event.user_id, f"Неверно введено значеие, введите заново")
+            elif k == 4:
+                try:
+                    years = int(request)
+                    if years >= ages[0] and years <= ages[1]:
+                        params["max_age"] = years
+                        print(params["max_age"])
+                        k += 1
+                        write_msg(event.user_id, f"Ищем людей по вашему запросу")
+                    else:
+                        write_msg(event.user_id, f"Неверно введено значеие, введите заново")
+                except ValueError:
+                    write_msg(event.user_id, f"Неверно введено значеие, введите заново")
--- vkapi.py
+++ vkapi.py
@@ -1,7 +1,25 @@
 import vk_api
+import requests
+from pprint import pprint
 
-token = ""
+
+
+token = "vk1.a.uvSWdZR6WWdEIOHoTLCI3x77cUVvGo1KCoIgo6iegN2edSNYCmmNBHht1FPs1KdbInQ16S5RP2Cl6PBJ89lQVFzWOflLk4rO1xlfv6AIRf_zp_1f256PJgpe4AjKmusezdsKdF6PuX7VcvDmD8hw1tqg9FzYhqxQ5WsEybDf1-n9KEnOB96CWD6Ln6eK4_TlINy5HKANPJwgQjmSB9gqsA"
+
 
 vk = vk_api.VkApi(token=token)
 
 
+params = {
+    "user_ids" : "1",
+    "access_token": token,
+    "v": "5.131"
+    }
+
+href = "https://api.vk.com/method/"
+
+req = requests.get(f"{href}users.get", params).json()
+
+pprint(req)
+
+print(int("d"))
